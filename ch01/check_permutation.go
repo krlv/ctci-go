7745b4b667571a44373fab8ab8eed76d39dcb940
krlv <eug.krlv@gmail.com>
2019-03-01 16:01:02 -0800

    Q1.2: Check Permutation, HashTable-based impementation

--- ch01/check_permutation.go
+++ ch01/check_permutation.go
@@ -1,13 +1,38 @@
 package ch01
 
-// CheckPermutation returns true if one string is a permutation of the other
-func CheckPermutation(s1 string, s2 string) bool {
+// CheckPermutationBySort returns true if one string is a permutation of the other
+// Sorts both strings and checks if they are equal
+func CheckPermutationBySort(s1 string, s2 string) bool {
 	if len(s1) != len(s2) {
 		return false
 	}
 
 	if SortString(s1) != SortString(s2) {
 		return false
+	}
+
+	return true
+}
+
+// CheckPermutationBySet returns true if one string is a permutation of the other
+// Use set of runes to reduce complexity
+func CheckPermutationBySet(s1 string, s2 string) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+
+	set := make(map[rune]int)
+
+	for _, c := range s1 {
+		set[c]++
+	}
+
+	for _, c := range s2 {
+		set[c]--
+
+		if set[c] < 0 {
+			return false
+		}
 	}
 
 	return true
