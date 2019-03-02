3b1344914e47ed6f0bc7ffbe3f8b7c847713c26a
krlv <eug.krlv@gmail.com>
2019-03-01 18:46:40 -0800

    Q1.3: URLify implementation for trimmed string

--- ch01/urlify.go
+++ ch01/urlify.go
@@ -1,7 +1,8 @@
 package ch01
 
-// URLify returns the string with HTML encoded spacese
-func URLify(s string, n int) string {
+// URLifyn returns the string with HTML encoded spacese
+// Expects real string length as a second parameter
+func URLifyn(s string, n int) string {
 	spaces := 0
 	for _, c := range s {
 		if c == ' ' {
@@ -13,6 +14,34 @@ func URLify(s string, n int) string {
 	runes := make([]rune, n+spaces*2)
 
 	for i := n - 1; i >= 0; i-- {
+		if s[i] == ' ' {
+			runes[index-1] = '0'
+			runes[index-2] = '2'
+			runes[index-3] = '%'
+			index = index - 3
+		} else {
+			runes[index-1] = rune(s[i])
+			index--
+		}
+	}
+
+	return string(runes)
+}
+
+// URLify returns the string with HTML encoded spacese
+// Expects trimmed URL string as a parameter
+func URLify(s string) string {
+	spaces := 0
+	for _, c := range s {
+		if c == ' ' {
+			spaces++
+		}
+	}
+
+	index := len(s) + spaces*2
+	runes := make([]rune, index)
+
+	for i := len(s) - 1; i >= 0; i-- {
 		if s[i] == ' ' {
 			runes[index-1] = '0'
 			runes[index-2] = '2'
--- ch01/urlify_test.go
+++ ch01/urlify_test.go
@@ -3,7 +3,14 @@ package ch01
 import "testing"
 
 func TestUrlifyn(t *testing.T) {
-	actual, expected := URLify("slug url", 8), "slug%20url"
+	actual, expected := URLifyn("slug url", 8), "slug%20url"
+	if actual != expected {
+		t.Errorf("Expected \"%s\" after ULRification, got \"%s\"", expected, actual)
+	}
+}
+
+func TestUrlify(t *testing.T) {
+	actual, expected := URLify("slug url"), "slug%20url"
 	if actual != expected {
 		t.Errorf("Expected \"%s\" after ULRification, got \"%s\"", expected, actual)
 	}
