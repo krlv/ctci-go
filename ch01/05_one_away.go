2a780bb3fe80d73f41fb5f0f34c0855b6dbe7d70
krlv <eug.krlv@gmail.com>
2019-03-03 17:07:46 -0800

    Q1.5: One Away, single loop implementation

--- ch01/05_one_away.go
+++ ch01/05_one_away.go
@@ -50,3 +50,38 @@ func isOneReplceAway(s1 string, s2 string) bool {
 
 	return true
 }
+
+// IsOneAwayLoop returns true if given strings are one (or zero) edits away
+func IsOneAwayLoop(s1 string, s2 string) bool {
+	s1len := len(s1)
+	s2len := len(s2)
+	diff := s1len - s2len
+
+	var i, j, edits int
+	for i < s1len && j < s2len {
+		if s1[i] != s2[j] {
+			edits++
+
+			if edits > 1 {
+				return false
+			}
+
+			if diff > 0 {
+				// s1 is longer than s2, move s2 index to a previous char
+				// so in the next iteration we will check s2[j] against s1[i++]
+				j--
+			}
+
+			if diff < 0 {
+				// s2 is longer than s1, move s1 index to a previous char
+				// so in the next iteration we will check s1[i] against s2[j++]
+				i--
+			}
+		}
+
+		i++ // move s1 index to the next char
+		j++ // move s2 index to the next char
+	}
+
+	return true
+}
--- ch01/05_one_away_test.go
+++ ch01/05_one_away_test.go
@@ -38,3 +38,40 @@ func TestIsOneAway(t *testing.T) {
 		t.Errorf("Strings \"%s\" and \"%s\" are more than one edit away", s1, s2)
 	}
 }
+
+func TestIsOneAwayLoop(t *testing.T) {
+	s1, s2 := "pale", "ple"
+	if !IsOneAwayLoop(s1, s2) {
+		t.Errorf("Strings \"%s\" and \"%s\" considered to be one edit away", s1, s2)
+	}
+
+	s1, s2 = "ple", "pale"
+	if !IsOneAwayLoop(s1, s2) {
+		t.Errorf("Strings \"%s\" and \"%s\" considered to be one edit away", s1, s2)
+	}
+
+	s1, s2 = "pales", "pale"
+	if !IsOneAwayLoop(s1, s2) {
+		t.Errorf("Strings \"%s\" and \"%s\" considered to be one edit away", s1, s2)
+	}
+
+	s1, s2 = "pale", "pales"
+	if !IsOneAwayLoop(s1, s2) {
+		t.Errorf("Strings \"%s\" and \"%s\" considered to be one edit away", s1, s2)
+	}
+
+	s1, s2 = "pale", "bale"
+	if !IsOneAwayLoop(s1, s2) {
+		t.Errorf("Strings \"%s\" and \"%s\" considered to be one edit away", s1, s2)
+	}
+
+	s1, s2 = "pale", "bake"
+	if IsOneAwayLoop(s1, s2) {
+		t.Errorf("Strings \"%s\" and \"%s\" are more than one edit away", s1, s2)
+	}
+
+	s1, s2 = "bak", "pale"
+	if IsOneAwayLoop(s1, s2) {
+		t.Errorf("Strings \"%s\" and \"%s\" are more than one edit away", s1, s2)
+	}
+}
