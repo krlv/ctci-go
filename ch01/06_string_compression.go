9fba270e0d804e6087597bc953aabf32871238be
krlv <eug.krlv@gmail.com>
2019-03-04 16:56:55 -0800

    Q1.5: String compression, bytes slice implementation

--- ch01/06_string_compression.go
+++ ch01/06_string_compression.go
@@ -31,3 +31,35 @@ func Compress(s string) string {
 
 	return s
 }
+
+// CompressBytes returns "compressed" string - all duplicates chars relpaced with "xN",
+// where x is original char and N is a number of chars
+// If compressed string isn't smaller, than original, original string will be returned
+func CompressBytes(s string) string {
+	slen := len(s)
+
+	// if len is less than 3, the original string will be smaller
+	if slen < 3 {
+		return s
+	}
+
+	bytes := make([]byte, 0)
+	count := 1
+	for i := 1; i < slen; i++ {
+		if s[i-1] != s[i] {
+			bytes = append(bytes, s[i-1])
+			bytes = append(bytes, []byte(strconv.Itoa(count))...)
+			count = 1
+		} else {
+			count++
+		}
+	}
+	bytes = append(bytes, s[slen-1])
+	bytes = append(bytes, []byte(strconv.Itoa(count))...)
+
+	if len(bytes) < slen {
+		return string(bytes)
+	}
+
+	return s
+}
--- ch01/06_string_compression_test.go
+++ ch01/06_string_compression_test.go
@@ -17,4 +17,31 @@ func TestCompress(t *testing.T) {
 	if actual != example {
 		t.Errorf("Expected to compress string \"%s\" to \"%s\", got \"%s\"", example, example, actual)
 	}
+
+	example = "aa"
+	actual = Compress(example)
+	if actual != example {
+		t.Errorf("Expected to compress string \"%s\" to \"%s\", got \"%s\"", example, example, actual)
+	}
+}
+
+func TestCompressBytes(t *testing.T) {
+	example := "aabcccccaaa"
+	expected := "a2b1c5a3"
+	actual := CompressBytes(example)
+	if actual != expected {
+		t.Errorf("Expected to compress string \"%s\" to \"%s\", got \"%s\"", example, expected, actual)
+	}
+
+	example = "abca"
+	actual = CompressBytes(example)
+	if actual != example {
+		t.Errorf("Expected to compress string \"%s\" to \"%s\", got \"%s\"", example, example, actual)
+	}
+
+	example = "aa"
+	actual = CompressBytes(example)
+	if actual != example {
+		t.Errorf("Expected to compress string \"%s\" to \"%s\", got \"%s\"", example, example, actual)
+	}
 }
