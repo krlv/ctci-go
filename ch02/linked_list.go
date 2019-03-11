998d430969df7ff0072cc6ccc9d8110e3b367584
krlv <eug.krlv@gmail.com>
2019-03-11 15:35:52 -0700

    Linked List implementation: delete node method

--- ch02/linked_list.go
+++ ch02/linked_list.go
@@ -2,22 +2,39 @@ package ch02
 
 // Node struct for linked list
 type Node struct {
-	value int
-	next  *Node
+	data int
+	next *Node
 }
 
 // New returns a new node
-func New(v int) *Node {
+func New(d int) *Node {
 	n := new(Node)
-	n.value = v
+	n.data = d
 	return n
 }
 
-// AppendToTail appends a new node with value v at the tail of list l
-func (node *Node) AppendToTail(v int) {
-	end := New(v)
+// AppendToTail appends a new node with data d at the tail of list l
+func (node *Node) AppendToTail(d int) {
+	end := New(d)
 	for node.next != nil {
 		node = node.next
 	}
 	node.next = end
+}
+
+// DeleteNode deletes node with data d from the list
+func (node *Node) DeleteNode(d int) {
+	for n := node; n != nil; n = n.next {
+		if n.data == d {
+			if n.next != nil {
+				n.data = n.next.data
+				n.next = n.next.next
+			} else {
+				n.data = 0
+				n.next = nil
+			}
+
+			return
+		}
+	}
 }
--- ch02/linked_list_test.go
+++ ch02/linked_list_test.go
@@ -5,14 +5,45 @@ import (
 )
 
 func TestAppendToTail(t *testing.T) {
-	n := New(1)
-	n.AppendToTail(2)
-	n.AppendToTail(3)
+	node := New(1)
+	node.AppendToTail(2)
+	node.AppendToTail(3)
 
-	for i, v := range []int{1, 2, 3} {
-		if n.value != v {
-			t.Errorf("%dth node expected to have value %v, got %v", i+1, v, n.value)
+	n := node
+	for i, d := range []int{1, 2, 3} {
+		if n.data != d {
+			t.Errorf("%dth node expected to have data %v, got %v", i+1, d, n.data)
 		}
 		n = n.next
+	}
+}
+
+func TestDeleteNode(t *testing.T) {
+	node := New(1)
+	node.AppendToTail(2)
+	node.AppendToTail(3)
+
+	node.DeleteNode(2)
+	n := node
+	for i, d := range []int{1, 3} {
+		if n.data != d {
+			t.Errorf("%dth node expected to have data %v, got %v\n", i+1, d, n.data)
+		}
+		n = n.next
+	}
+
+	node.DeleteNode(1)
+	n = node
+	for i, d := range []int{3} {
+		if n.data != d {
+			t.Errorf("%dth node expected to have data %v, got %v\n", i+1, d, n.data)
+		}
+		n = n.next
+	}
+
+	node.DeleteNode(3)
+	n = node
+	if n.data != 0 || n.next != nil {
+		t.Errorf("Expected to have empty linked list, got %v\n", n)
 	}
 }
