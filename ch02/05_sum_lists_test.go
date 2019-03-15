0ee0984cb240442862e55fe352eb863d67111ab6
krlv <eug.krlv@gmail.com>
2019-03-14 19:02:56 -0700

    Q2.5: Improve code coverage

--- ch02/05_sum_lists_test.go
+++ ch02/05_sum_lists_test.go
@@ -138,4 +138,23 @@ func TestSumListsReverse(t *testing.T) {
 		}
 		actual = actual.next
 	}
+
+	n1 = New(1)
+	n1.AppendToTail(1)
+	n1.AppendToTail(1)
+
+	n2 = New(2)
+	n2.AppendToTail(2)
+	n2.AppendToTail(2)
+	n2.AppendToTail(2)
+
+	expected = []int{2, 3, 3, 3}
+	actual = SumListsReverse(n1, n2)
+
+	for i := 0; actual != nil; i++ {
+		if actual.data != expected[i] {
+			t.Errorf("%dth node expected to have data %v, got %v", i+1, expected[i], actual.data)
+		}
+		actual = actual.next
+	}
 }
