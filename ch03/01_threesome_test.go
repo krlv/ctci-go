45caf2e3312e7a7903d1383f748914386f7910e7
krlv <eug.krlv@gmail.com>
2019-03-26 08:33:06 -0700

    #1 3.1 Three in One: test for IsEmpty method

--- ch03/01_threesome_test.go
+++ ch03/01_threesome_test.go
@@ -172,5 +172,47 @@ func TestNthPeek(t *testing.T) {
 }
 
 func TestNthIsEmpty(t *testing.T) {
-	// TBD
+	n, m := 3, 2
+	nth := new(NthStack)
+
+	nth.n = n
+	nth.storage = make([]int, n*(m+1))
+	nth.start = make([]int, n)
+	nth.len = make([]int, n)
+	nth.cap = make([]int, n)
+
+	for i := 0; i < n; i++ {
+		nth.start[i] = i * (m + 1)
+		nth.len[i] = 0
+		nth.cap[i] = 1
+	}
+
+	for i := 0; i < n; i++ {
+		if !nth.IsEmpty(i) {
+			t.Errorf("%dth stack expected to be empty\n", (i + 1))
+		}
+	}
+
+	values := [][]int{
+		{10, 1},
+		{20, 2},
+		{30, 3},
+	}
+
+	for i := 0; i < n; i++ {
+		start := i * (m + 1)
+		stop := start + m
+
+		for j := start; j < stop; j++ {
+			nth.storage[j] = values[i][j-start]
+			nth.len[i]++
+			nth.cap[i]++
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		if nth.IsEmpty(i) {
+			t.Errorf("%dth stack expected to be non-empty\n", (i + 1))
+		}
+	}
 }
