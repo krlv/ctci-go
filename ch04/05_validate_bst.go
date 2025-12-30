8385b208c6312f5be489388203c8e4bd62f51ed3
krlv <eug.krlv@gmail.com>
2025-12-29 18:23:56 -0800

    feat(ch04): Q4.5 Validate BST (min/max recursion)

--- ch04/05_validate_bst.go
+++ ch04/05_validate_bst.go
@@ -33,3 +33,29 @@ func IsValidBSTInOrder[T cmp.Ordered](root *BinaryTreeNode[T]) bool {
 
 	return inorder(root)
 }
+
+// validateMinMax checks whether the binary subtree satisfies the binary search
+// tree (BST) property given exclusive `min` and `max` bounds.
+// For each node, the function ensures `min < node.Data < max` and recursively
+// enforces appropriate bounds for left and right subtrees.
+//
+// Time complexity: O(n) — each node is visited once.
+// Space complexity: O(1) excluding recursion stack.
+// Auxiliary space: O(h) — recursion stack proportional to tree height h.
+func validateMinMax[T cmp.Ordered](root *BinaryTreeNode[T], min, max *T) bool {
+	if root == nil {
+		return true
+	}
+
+	if (min != nil && root.Data <= *min) && (max != nil && root.Data >= *max) {
+		return false
+	}
+
+	return validateMinMax(root.Left, min, &root.Data) && validateMinMax(root.Right, &root.Data, max)
+}
+
+// IsValidBSTMinMax reports whether the binary tree is a valid BST.
+// It uses the min/max recursion strategy and starts without bounds.
+func IsValidBSTMinMax[T cmp.Ordered](root *BinaryTreeNode[T]) bool {
+	return validateMinMax(root, nil, nil)
+}
--- ch04/05_validate_bst_test.go
+++ ch04/05_validate_bst_test.go
@@ -56,3 +56,55 @@ func TestIsValidBSTInOrder(t *testing.T) {
 		})
 	}
 }
+
+func TestIsValidBSTMinMax(t *testing.T) {
+	type testCase[T cmp.Ordered] struct {
+		name string
+		root *BinaryTreeNode[T]
+		want bool
+	}
+	tests := []testCase[int]{
+		{
+			name: "empty tree",
+			root: nil,
+			want: true,
+		},
+		{
+			name: "2 nodes invalid BST",
+			root: &BinaryTreeNode[int]{
+				Data:  20,
+				Right: &BinaryTreeNode[int]{Data: 20},
+			},
+			want: false,
+		},
+		{
+			name: "3 nodes valid",
+			root: &BinaryTreeNode[int]{
+				Data:  2,
+				Left:  &BinaryTreeNode[int]{Data: 1},
+				Right: &BinaryTreeNode[int]{Data: 3},
+			},
+			want: true,
+		},
+		{
+			name: "5 nodes invalid",
+			root: &BinaryTreeNode[int]{
+				Data: 5,
+				Left: &BinaryTreeNode[int]{Data: 1},
+				Right: &BinaryTreeNode[int]{
+					Data:  4,
+					Left:  &BinaryTreeNode[int]{Data: 3},
+					Right: &BinaryTreeNode[int]{Data: 6},
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsValidBSTMinMax(tt.root); got != tt.want {
+				t.Errorf("IsValidBSTMinMax() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
